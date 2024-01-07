e2a5e316e8483374be1900abac69cdf75975f9e7
EggTV <ssh521@naver.com>
2024-01-08 00:43:11 +0900

    로그인 디자인 수정

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
 
+<x-auth-card>
+
     <div class="w-full mx-auto max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
         <form class="space-y-6" action="{{ route('password.confirm') }}" method="POST">
             @csrf
@@ -16,5 +18,7 @@
             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">비밀번호 확인하기</button>
         </form>
     </div>    
+
+</x-auth-card>
 
 @endsection
